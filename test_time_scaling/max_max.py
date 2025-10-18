6a0a2c9d32207f4145ac3619ccfb031ca168e97f
yoshility <90372442+yoshility@users.noreply.github.com>
2025-10-18 07:55:04 +0000

    implement max_max depth=2 and debug tool

--- test_time_scaling/max_max.py
+++ test_time_scaling/max_max.py
@@ -2,11 +2,14 @@ import copy
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# model and tokenizer
+# model and tokenizer (global)
 model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float16, device_map="auto")
 model.eval()
+
+# 現在のステップ数
+num_step = 0
 
 def top_k_logits(logits, k):
     v, ix = torch.topk(logits, k)
@@ -15,7 +18,6 @@ def top_k_logits(logits, k):
     return out
 
 def generate_one_step(step) -> str:
-    print("----------- Start generate_one_step -----------")
     # stepはsystem,user(,assistant)をすべて含む
     # これに対してダブル改行が出るまで続きを生成する
     input_text = tokenizer.apply_chat_template(
@@ -52,9 +54,9 @@ def generate_one_step(step) -> str:
         generated_ids = torch.cat([generated_ids, next_token_id], dim=-1)
         
         # decode to text (ちゃんとダブル改行検知できてるかチェック)
-        decoded_next_token_id = tokenizer.decode(next_token_id[0])
-        enter_exist = '\n\n' in decoded_next_token_id
-        print(f"[{decoded_next_token_id}]:[{enter_exist}]", end=" ")
+        # decoded_next_token_id = tokenizer.decode(next_token_id[0])
+        # enter_exist = '\n\n' in decoded_next_token_id
+        # print(f"[{decoded_next_token_id}]:[{enter_exist}]", end=" ")
 
         # if \n\n or EOS, stop generation
         if "\n\n" in tokenizer.decode(next_token_id[0]):
@@ -78,7 +80,7 @@ def static_value(step: str) -> float:
     return 1.0
 
 # min-max -> max-max
-def max_max(step: str, depth: int):
+def max_max(step: str, child_id: int, depth: int):
     print(f"\n[Start max-max (depth={depth})]\n")
     # 読み深さに達した
     if depth == 0:
@@ -88,43 +90,47 @@ def max_max(step: str, depth: int):
     children = []
     tokenized_children = []
     for i in range(3):
-        print(f"\n[generate children[{i}]]\n")
+        print(f"\n[generate children[{child_id+str(i)}]]\n")
         next_step, tokenized_next_step = generate_one_step(step)
         children.append(next_step)
         tokenized_children.append(tokenized_next_step)
+        print(f"\nchildren[{child_id+str(i)}]:\n{children[i]}\n")
     
     max_score = -1e4
     max_index = 0
     for i in range(3):
-        print(f"\n[max_max(children[{i}])]\n")
-        score, _, _ = max_max(children[i], depth-1)
+        print(f"\n[max_max(children[{child_id+str(i)}])]\n")
+        score, _, _ = max_max(children[i], child_id+str(i), depth-1)
         if score > max_score:
             max_score = score
             max_index = i
     
     return max_score, children[max_index], tokenized_children[max_index]
 
-# 次のステップを決める
+# 繰り返し次のステップを決めていく
+if __name__ == '__main__':
+    # 初期化
+    # use chat template -> avoid hallucination
+    # input
+    prompt = "Natalia sold clips to 48 of her friends in April, and then she sold half as many clips in May. How many clips did Natalia sell altogether in April and May?"
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant solving math problems."},
+        {"role": "user", "content": prompt}
+    ]
+    now_step = messages
 
-# 初期化
-# use chat template -> avoid hallucination
-# input
-prompt = "Natalia sold clips to 48 of her friends in April, and then she sold half as many clips in May. How many clips did Natalia sell altogether in April and May?"
-messages = [
-    {"role": "system", "content": "You are a helpful assistant solving math problems."},
-    {"role": "user", "content": prompt}
-]
-now_step = messages
-
-# 生成が終わるまでmin-maxを繰り返す
-while True:
-    _, next_step, tokenized_next_step = max_max(
-        step=now_step,
-        depth=2
-    )
-    now_step = next_step
-    print(f"\nnext_step:\n{next_step}\n")
-    # もし選んだ次ステップでEOSなら、生成終了
-    if tokenized_next_step[0][-1] == tokenizer.eos_token_id:
-        print(f"\n生成すべて完了\n")
-        break
+    # 生成が終わるまでmax-maxを繰り返す
+    while True:
+        num_step += 1
+        print(f"\n<<num_step: {num_step}>>\n")
+        _, next_step, tokenized_next_step = max_max(
+            step=now_step,
+            child_id=f"{num_step}-C", # for debug, assign an id to each node
+            depth=1
+        )
+        now_step = next_step
+        print(f"\nchosen_next_step:\n{next_step}\n")
+        # もし選んだ次ステップでEOSなら、生成終了
+        if tokenized_next_step[0][-1] == tokenizer.eos_token_id:
+            print(f"\n生成すべて完了\n")
+            break
